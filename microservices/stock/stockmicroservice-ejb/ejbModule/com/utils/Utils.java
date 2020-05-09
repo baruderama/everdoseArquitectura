0d56aa7b9551bedb78a56470c0433b471ddffd61
valntinaf <feruereru@gmail.com>
2020-05-09 10:01:24 +0000

    Dependencies added in pom

--- microservices/stock/stockmicroservice-ejb/ejbModule/com/utils/Utils.java
+++ microservices/stock/stockmicroservice-ejb/ejbModule/com/utils/Utils.java
@@ -1,7 +1,5 @@
 package com.utils;
 
-import java.net.http.HttpClient;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
